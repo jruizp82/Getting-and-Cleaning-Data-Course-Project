ed86b074f3c0bbe35416c7d9e8b1a7143db97a6d
Juan Antonio Ruiz <ruiz.ja.1982@gmail.com>
2015-10-25 18:16:22 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,15 @@
-# # Download the file and put the file in the data folder
-# if(!file.exists("./data")){dir.create("./data")}
-# fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-# download.file(fileUrl,destfile="./data/Dataset.zip")
-# 
-# # Unzip the file
-# unzip(zipfile="./data/Dataset.zip",exdir="./data")
-# 
-# # Unzipped files are in the folderUCI HAR Dataset. Get the list of the files
-# path_rf <- file.path("./data" , "UCI HAR Dataset")
-# files<-list.files(path_rf, recursive=TRUE)
+# Download the file and put the file in the data folder
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+ 
+# Unzip the file
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+
+# Unzipped files are in the folderUCI HAR Dataset. Get the list of the files
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
 
 # Read the Activity files
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
